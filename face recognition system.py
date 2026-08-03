6a018107956dc5380ce17f5ba1685c89a7b53c85
atheena <dilansampath200508@gmail.com>
2026-08-03 18:39:24 +0530

    input known face for the system

--- face recognition system.py
+++ face recognition system.py
@@ -5,3 +5,5 @@ import face_recognition as fr
 vide_capture = cv2.VideoCapture(0)
 image = fr.load_image_file(r"E:\Python Project\ML-projects-\58c860a42c00002000fee66c.webp")
 image_face_encoding = fr.face_encodings(image)[0]
+known_face_encodings = [image_face_encoding]
+known_face_names = ["dilan"]
