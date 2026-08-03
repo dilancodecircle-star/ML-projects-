a3efc4cb26f1f38242dc874ac275bb37f791f45f
atheena <dilansampath200508@gmail.com>
2026-08-03 15:25:57 +0530

    feat: implement face and object detection script using YOLO models

---
+++ face_detection.py
@@ -0,0 +1,14 @@
+import cv2
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt")
+face_model = YOLO("yolov8m-face.pt")
+photo = cv2.imread("Woman-cuddling-with-cat-1536x864.jpg")
+result = model(photo)
+face_result = face_model(photo)
+cat_person = result[0].plot()
+cat_person_face = face_result[0].plot(img = cat_person)
+
+
+cv2.imshow("my window" , result[0].plot())
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
