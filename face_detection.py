967bb19d05e55a5ab969f4ca79be6e5442c0824e
atheena <dilansampath200508@gmail.com>
2026-08-03 15:26:32 +0530

    feat: implement real-time object detection loop using webcam input

--- face_detection.py
+++ face_detection.py
@@ -12,3 +12,18 @@ cat_person_face = face_result[0].plot(img = cat_person)
 cv2.imshow("my window" , result[0].plot())
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
+model = YOLO("yolov8n.pt")
+face_model = YOLO("yolov8m-face.pt")
+while cv2.waitKey(1)!=ord("x"):
+    _, frame = cap.read()
+    result = model(frame , verbose = False)
+   # face_result = face_model(frame , verbose = False )
+    cat_person = result[0].plot()
+   # cat_person_face = face_result[0].plot(img = cat_person)
+
+    cv2.imshow("my window" , cat_person)
+    
+cv2.waitKey(5000)
+cap.release()
+cv2.destroyAllWindows()
