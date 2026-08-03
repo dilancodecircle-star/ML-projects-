b7658c0b3a6a3787641df9fe5d2e82e8bf07a11c
atheena <dilansampath200508@gmail.com>
2026-08-03 18:36:20 +0530

    use images to model

--- face recognition system.py
+++ face recognition system.py
@@ -1,3 +1,7 @@
 import cv2
 import numpy as np
 import face_recognition as fr
+
+vide_capture = cv2.VideoCapture(0)
+image = fr.load_image_file(r"E:\Python Project\ML-projects-\58c860a42c00002000fee66c.webp")
+image_face_encoding = fr.face_encodings(image)[0]
