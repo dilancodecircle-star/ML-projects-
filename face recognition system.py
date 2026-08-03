9bd99a721fce6f7603c83789059d85c2b77a6d8c
atheena <dilansampath200508@gmail.com>
2026-08-03 19:53:28 +0530

    fix the error

--- face recognition system.py
+++ face recognition system.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
 import face_recognition as fr
-
+import time
+vide_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+time.sleep(2)
 vide_capture = cv2.VideoCapture(0)
-image = fr.load_image_file(r"E:\Python Project\ML-projects-\58c860a42c00002000fee66c.webp")
+image = fr.load_image_file(r"E:\\Python Project\\ML-projects-\\58c860a42c00002000fee66c.webp")
 image_face_encoding = fr.face_encodings(image)[0]
 known_face_encodings = [image_face_encoding]
 known_face_names = ["dilan"]
 
 while True:
     ret , frame = vide_capture.read()
+    if not ret or frame is None:
+        continue
     rgb_frame = frame[: , : , ::-1]
 
     fc_locations = fr.face_locations(rgb_frame)
