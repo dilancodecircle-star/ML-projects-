dfab0e8a193acbc9bc4e51867332458d9b8f2f97
atheena <dilansampath200508@gmail.com>
2026-08-03 10:01:16 +0530

    download data

--- stock price predictor.py
+++ stock price predictor.py
@@ -4,4 +4,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+stocks  = input("Enter the code of stock : ")
+data = yf.download(stocks , start = "2020-01-01" , end = "2026-01-01")
+data.head()
+
+
 
