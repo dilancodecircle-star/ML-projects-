bd135564f987e7210febaa2f6bfe9153855f0322
atheena <dilansampath200508@gmail.com>
2026-08-03 11:42:12 +0530

    make svm kernael

--- stock price predictor.py
+++ stock price predictor.py
@@ -6,6 +6,10 @@ import yfinance as yf
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error , r2_score
+from sklearn.linear_model import Lasso , Ridge
+from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV
+
 stocks  = input("Enter the code of stock : ")
 data = yf.download(stocks , start = "2020-01-01" , end = "2026-01-01" , auto_adjust = True)
 print(data.head())
@@ -49,4 +53,27 @@ def calculate_metrics(y_test , y_pred):
     print("R-squared (R2):", r2)
 
 calculate_metrics(y_test , predict1)
+ls = Lasso().fit(x_train , y_train)
+ridge = Ridge().fit(x_train , y_train)
 
+la_predict = ls.predict(x_test)
+ri_predict = ridge.predict(x_test)
+print("Lasso Regression Predictions:", la_predict)
+print("Ridge Regression Predictions:", ri_predict)
+
+calculate_metrics(y_test , la_predict)
+
+print("ridge errors")
+calculate_metrics(y_test , ri_predict)
+
+svr = SVR()
+param_grid = {'C' :[0.1 , 1 , 10 , 1000]  , 
+              'gamma' : [1 , 0.1 , 0.01 , 0.001 , 0.0001] , 
+              'kernel' : ['rbf']}
+
+grid = GridSearchCV(SVR() , param_grid , refit = True , verbose = 3)
+grid.fit(x_train , y_train)
+
+svr  = SVR(C = 10 , gamma = 0.01 , kernel = 'rbf')
+svr.fit(x_train , y_train)
+svr_pred = svr.predict(x_test)
